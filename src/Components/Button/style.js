e27fcf6ec46d749822a127cdefbd99677f572257
João Victor <santosjoao.dev@gmail.com>
2021-08-03 19:40:04 -0300

    create button component

---
+++ src/Components/Button/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonElement = styled.button`
+  border-style: none;
+  background: #3C4262;
+  font-weight: 700;
+  font-size: 2rem;
+  border-radius: 5px;
+  padding: .5rem 2.5rem;
+  box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.4);
+
+  &:hover{
+    box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.5);
+  }
+  cursor: pointer;
+`;
